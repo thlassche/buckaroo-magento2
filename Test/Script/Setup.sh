48d232877c88ed9f8cab87ec5a112462ea97537b
Mickey Beijer <mickey.beijer@tig.nl>
2019-01-10 11:40:34 +0100

    BUCKM2-337 : fixed bin/magento usage

--- Test/Script/Setup.sh
+++ Test/Script/Setup.sh
@@ -57,9 +57,9 @@ chmod 777 "${BUILD_DIR}/vendor/phpunit/phpunit/phpunit"
 cd ${BUILD_DIR}
 
 if [ "$CODE_COVERAGE" = "false" ]; then
-    cd ${BUILD_DIR} && bin/magento setup:di:compile;
+    cd ${BUILD_DIR} && php -d memory_limit=2048M bin/magento setup:di:compile;
 fi
 
-cd ${BUILD_DIR} && bin/magento i18n:collect-phrases vendor/tig/buckaroo-travis
+cd ${BUILD_DIR} && php -d memory_limit=2048M bin/magento i18n:collect-phrases vendor/tig/buckaroo-travis
 
 "${BUILD_DIR}/vendor/phpunit/phpunit/phpunit" -c "${BUILD_DIR}/vendor/tig/buckaroo-travis/phpunit.xml.dist" --testsuite unit
