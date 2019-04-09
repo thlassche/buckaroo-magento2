36783d535d460055a039bb67411857103f469a29
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2019-04-09 15:18:46 +0200

    Added base of creditcards payment method

---
+++ Model/ConfigProvider/Method/Creditcards.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license   http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+
+namespace TIG\Buckaroo\Model\ConfigProvider\Method;
+
+/**
+ * @method getPaymentFeeLabel()
+ * @method getSellersProtection()
+ * @method getSellersProtectionEligible()
+ * @method getSellersProtectionIneligible()
+ * @method getSellersProtectionItemnotreceivedEligible()
+ * @method getSellersProtectionUnauthorizedpaymentEligible()
+ */
+class Creditcards extends AbstractConfigProvider
+{
+    const XPATH_CREDITCARDS_PAYMENT_FEE                      = 'payment/tig_buckaroo_creditcards/payment_fee';
+    const XPATH_CREDITCARDS_PAYMENT_FEE_LABEL                = 'payment/tig_buckaroo_creditcards/payment_fee_label';
+    const XPATH_CREDITCARDS_ACTIVE                           = 'payment/tig_buckaroo_creditcards/active';
+    const XPATH_CREDITCARDS_ACTIVE_STATUS                    = 'payment/tig_buckaroo_creditcards/active_status';
+    const XPATH_CREDITCARDS_ORDER_STATUS_SUCCESS             = 'payment/tig_buckaroo_creditcards/order_status_success';
+    const XPATH_CREDITCARDS_ORDER_STATUS_FAILED              = 'payment/tig_buckaroo_creditcards/order_status_failed';
+    const XPATH_CREDITCARDS_AVAILABLE_IN_BACKEND             = 'payment/tig_buckaroo_creditcards/available_in_backend';
+    const XPATH_CREDITCARDS_SELLERS_PROTECTION               = 'payment/tig_buckaroo_creditcards/sellers_protection';
+    const XPATH_CREDITCARDS_SELLERS_PROTECTION_ELIGIBLE      = 'payment/tig_buckaroo_creditcards/sellers_protection_eligible';
+    const XPATH_CREDITCARDS_SELLERS_PROTECTION_INELIGIBLE    = 'payment/tig_buckaroo_creditcards/sellers_protection_ineligible';
+    const XPATH_CREDITCARDS_SELLERS_PROTECTION_ITEMNOTRECEIVED_ELIGIBLE = 'payment/tig_buckaroo_creditcards/sellers_protection_itemnotreceived_eligible';
+    const XPATH_CREDITCARDS_SELLERS_PROTECTION_UNAUTHORIZEDPAYMENT_ELIGIBLE = 'payment/tig_buckaroo_creditcards/sellers_protection_unauthorizedpayment_eligible';
+
+    const XPATH_ALLOWED_CURRENCIES = 'payment/tig_buckaroo_creditcards/allowed_currencies';
+
+    const XPATH_ALLOW_SPECIFIC                  = 'payment/tig_buckaroo_creditcards/allowspecific';
+    const XPATH_SPECIFIC_COUNTRY                = 'payment/tig_buckaroo_creditcards/specificcountry';
+
+    /**
+     * @return array|void
+     */
+    public function getConfig()
+    {
+        $paymentFeeLabel = $this->getBuckarooPaymentFeeLabel(\TIG\Buckaroo\Model\Method\Creditcards::PAYMENT_METHOD_CODE);
+
+        return [
+            'payment' => [
+                'buckaroo' => [
+                    'creditcards' => [
+                        'paymentFeeLabel' => $paymentFeeLabel,
+                        'allowedCurrencies' => $this->getAllowedCurrencies(),
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @param null|int $storeId
+     *
+     * @return float
+     */
+    public function getPaymentFee($storeId = null)
+    {
+        $paymentFee = $this->scopeConfig->getValue(
+            self::XPATH_CREDITCARDS_PAYMENT_FEE,
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $storeId
+        );
+
+        return $paymentFee ? $paymentFee : false;
+    }
+}
---
+++ Model/Method/Creditcards.php
@@ -0,0 +1,377 @@
+<?php
+/**
+ *                  ___________       __            __
+ *                  \__    ___/____ _/  |_ _____   |  |
+ *                    |    |  /  _ \\   __\\__  \  |  |
+ *                    |    | |  |_| ||  |   / __ \_|  |__
+ *                    |____|  \____/ |__|  (____  /|____/
+ *                                              \/
+ *          ___          __                                   __
+ *         |   |  ____ _/  |_   ____ _______   ____    ____ _/  |_
+ *         |   | /    \\   __\_/ __ \\_  __ \ /    \ _/ __ \\   __\
+ *         |   ||   |  \|  |  \  ___/ |  | \/|   |  \\  ___/ |  |
+ *         |___||___|  /|__|   \_____>|__|   |___|  / \_____>|__|
+ *                  \/                           \/
+ *                  ________
+ *                 /  _____/_______   ____   __ __ ______
+ *                /   \  ___\_  __ \ /  _ \ |  |  \\____ \
+ *                \    \_\  \|  | \/|  |_| ||  |  /|  |_| |
+ *                 \______  /|__|    \____/ |____/ |   __/
+ *                        \/                       |__|
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Creative Commons License.
+ * It is available through the world-wide-web at this URL:
+ * http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ * If you are unable to obtain it through the world-wide-web, please send an email
+ * to servicedesk@tig.nl so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future. If you wish to customize this module for your
+ * needs please contact servicedesk@tig.nl for more information.
+ *
+ * @copyright Copyright (c) Total Internet Group B.V. https://tig.nl/copyright
+ * @license   http://creativecommons.org/licenses/by-nc-nd/3.0/nl/deed.en_US
+ */
+
+namespace TIG\Buckaroo\Model\Method;
+
+class Creditcards extends AbstractMethod
+{
+    /**
+     * Payment Code
+     */
+    const PAYMENT_METHOD_CODE = 'tig_buckaroo_creditcards';
+
+    /**
+     * @var string
+     */
+    public $buckarooPaymentMethodCode = 'creditcards';
+
+    // @codingStandardsIgnoreStart
+    /**
+     * Payment method code
+     *
+     * @var string
+     */
+    protected $_code = self::PAYMENT_METHOD_CODE;
+
+    /**
+     * @var bool
+     */
+    protected $_isGateway               = true;
+
+    /**
+     * @var bool
+     */
+    protected $_canOrder                = true;
+
+    /**
+     * @var bool
+     */
+    protected $_canAuthorize            = false;
+
+    /**
+     * @var bool
+     */
+    protected $_canCapture              = false;
+
+    /**
+     * @var bool
+     */
+    protected $_canCapturePartial       = false;
+
+    /**
+     * @var bool
+     */
+    protected $_canRefund               = true;
+
+    /**
+     * @var bool
+     */
+    protected $_canVoid                 = true;
+
+    /**
+     * @var bool
+     */
+    protected $_canUseInternal          = true;
+
+    /**
+     * @var bool
+     */
+    protected $_canUseCheckout          = true;
+
+    /**
+     * @var bool
+     */
+    protected $_canRefundInvoicePartial = true;
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * @var bool
+     */
+    public $usesRedirect                = false;
+
+    /** @var \TIG\Buckaroo\Service\CreditManagement\ServiceParameters */
+    private $serviceParameters;
+
+    public function __construct(
+        \Magento\Framework\ObjectManagerInterface $objectManager,
+        \Magento\Framework\Model\Context $context,
+        \Magento\Framework\Registry $registry,
+        \Magento\Framework\Api\ExtensionAttributesFactory $extensionFactory,
+        \Magento\Framework\Api\AttributeValueFactory $customAttributeFactory,
+        \Magento\Payment\Helper\Data $paymentData,
+        \Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig,
+        \Magento\Payment\Model\Method\Logger $logger,
+        \Magento\Developer\Helper\Data $developmentHelper,
+        \TIG\Buckaroo\Service\CreditManagement\ServiceParameters $serviceParameters,
+        \Magento\Framework\Model\ResourceModel\AbstractResource $resource = null,
+        \Magento\Framework\Data\Collection\AbstractDb $resourceCollection = null,
+        \TIG\Buckaroo\Gateway\GatewayInterface $gateway = null,
+        \TIG\Buckaroo\Gateway\Http\TransactionBuilderFactory $transactionBuilderFactory = null,
+        \TIG\Buckaroo\Model\ValidatorFactory $validatorFactory = null,
+        \TIG\Buckaroo\Helper\Data $helper = null,
+        \Magento\Framework\App\RequestInterface $request = null,
+        \TIG\Buckaroo\Model\RefundFieldsFactory $refundFieldsFactory = null,
+        \TIG\Buckaroo\Model\ConfigProvider\Factory $configProviderFactory = null,
+        \TIG\Buckaroo\Model\ConfigProvider\Method\Factory $configProviderMethodFactory = null,
+        \Magento\Framework\Pricing\Helper\Data $priceHelper = null,
+        array $data = []
+    ) {
+        parent::__construct(
+            $objectManager,
+            $context,
+            $registry,
+            $extensionFactory,
+            $customAttributeFactory,
+            $paymentData,
+            $scopeConfig,
+            $logger,
+            $developmentHelper,
+            $resource,
+            $resourceCollection,
+            $gateway,
+            $transactionBuilderFactory,
+            $validatorFactory,
+            $helper,
+            $request,
+            $refundFieldsFactory,
+            $configProviderFactory,
+            $configProviderMethodFactory,
+            $priceHelper,
+            $data
+        );
+
+        $this->serviceParameters = $serviceParameters;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getOrderTransactionBuilder($payment)
+    {
+        $transactionBuilder = $this->transactionBuilderFactory->get('order');
+
+        $services = [];
+        $services[] = $this->getCreditcardsService($payment);
+
+        $filterParameter = [
+            ['Name' => 'AllowedServices'],
+            ['Name' => 'Gender', 'Group' => 'Person']
+        ];
+
+        /**
+         * Buckaroo Push is send before Response, for correct flow we skip the first push
+         * @todo when buckaroo changes the push / response order this can be removed
+         */
+        $payment->setAdditionalInformation('skip_push', 1);
+
+        $cmService = $this->serviceParameters->getCreateCombinedInvoice($payment, 'creditcards', $filterParameter);
+        if (count($cmService) > 0) {
+            $services[] = $cmService;
+
+            $payment->setAdditionalInformation(
+                'skip_push', 2
+            );
+        }
+
+        /** @var \TIG\Buckaroo\Model\ConfigProvider\Method\Creditcards $creditcardsConfig */
+        $transactionBuilder->setOrder($payment->getOrder())
+            ->setServices($services)
+            ->setMethod('TransactionRequest');
+
+        return $transactionBuilder;
+    }
+
+    /**
+     * @param \Magento\Sales\Api\Data\OrderPaymentInterface|\Magento\Payment\Model\InfoInterface $payment
+     *
+     * @return array
+     */
+    public function getCreditcardsService($payment)
+    {
+        /** @var \TIG\Buckaroo\Model\ConfigProvider\Method\Creditcards $creditcardsConfig */
+        $creditcardsConfig = $this->configProviderMethodFactory->get('creditcards');
+
+        $dueDays = abs($creditcardsConfig->getDueDate());
+
+        $now = new \DateTime();
+        $now->modify('+' . $dueDays . ' day');
+
+        /**@var \Magento\Sales\Model\Order\Address $billingAddress */
+        $billingAddress = $payment->getOrder()->getBillingAddress();
+
+        $services = [
+            'Name'             => 'creditcards',
+            'Action'           => 'Pay',
+            'Version'          => 2,
+            'RequestParameter' => [
+                [
+                    '_'    => $billingAddress->getFirstname(),
+                    'Name' => 'CustomerFirstName',
+                ],
+                [
+                    '_'    => $billingAddress->getLastname(),
+                    'Name' => 'CustomerLastName',
+                ],
+                [
+                    '_'    => $billingAddress->getCountryId(),
+                    'Name' => 'CustomerCountry',
+                ],
+                [
+                    '_'    => $payment->getOrder()->getCustomerEmail(),
+                    'Name' => 'CustomerEmail',
+                ],
+                [
+                    '_'    => $now->format('Y-m-d'),
+                    'Name' => 'DateDue'
+                ],
+                [
+                    '_'    => $creditcardsConfig->getSendEmail(),
+                    'Name' => 'SendMail'
+                ]
+            ],
+        ];
+
+        return $services;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function canProcessPostData($payment, $postData)
+    {
+        $orderState = $payment->getOrder()->getState();
+        if ($orderState == \Magento\Sales\Model\Order::STATE_PROCESSING && $postData['brq_statuscode'] == "792") {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function afterOrder($payment, $response)
+    {
+        if (empty($response[0]->Services->Service)) {
+            return parent::afterOrder($payment, $response);
+        }
+
+        $invoiceKey = '';
+        $services = $response[0]->Services->Service;
+
+        if (!is_array($services)) {
+            $services = [$services];
+        }
+
+        foreach ($services as $service) {
+            if ($service->Name == 'CreditManagement3' && $service->ResponseParameter->Name == 'InvoiceKey') {
+                $invoiceKey = $service->ResponseParameter->_;
+            }
+        }
+
+        if (strlen($invoiceKey) > 0) {
+            $payment->setAdditionalInformation('buckaroo_cm3_invoice_key', $invoiceKey);
+        }
+
+        return parent::afterOrder($payment, $response);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getCaptureTransactionBuilder($payment)
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAuthorizeTransactionBuilder($payment)
+    {
+        return false;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getRefundTransactionBuilder($payment)
+    {
+        $transactionBuilder = $this->transactionBuilderFactory->get('refund');
+
+        $services = [
+            'Name'    => 'creditcards',
+            'Action'  => 'Refund',
+            'Version' => 1,
+        ];
+
+        $requestParams = $this->addExtraFields($this->_code);
+        $services = array_merge($services, $requestParams);
+
+        /**
+         * @noinspection PhpUndefinedMethodInspection
+         */
+        $transactionBuilder->setOrder($payment->getOrder())
+            ->setServices($services)
+            ->setMethod('TransactionRequest')
+            ->setOriginalTransactionKey(
+                $payment->getAdditionalInformation(self::BUCKAROO_ORIGINAL_TRANSACTION_KEY_KEY)
+            );
+
+        return $transactionBuilder;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getVoidTransactionBuilder($payment)
+    {
+        $services = $this->serviceParameters->getCreateCreditNote($payment);
+
+        if (count($services) <= 0) {
+            return true;
+        }
+
+        $transactionBuilder = $this->transactionBuilderFactory->get('order');
+
+        $transactionBuilder->setOrder($payment->getOrder())
+            ->setAmount(0)
+            ->setType('void')
+            ->setServices($services)
+            ->setMethod('DataRequest')
+            ->setInvoiceId($payment->getOrder()->getIncrementId() . '-creditnote')
+            ->setOriginalTransactionKey(
+                $payment->getAdditionalInformation(
+                    self::BUCKAROO_ORIGINAL_TRANSACTION_KEY_KEY
+                )
+            );
+
+        return $transactionBuilder;
+    }
+}
