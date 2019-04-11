690784219067b315dc91ecb4f84d01772b13511e
Dennis van der Hammen <dennis.van.der.hammen@tig.nl>
2019-04-11 16:52:18 +0200

    Added required fields for creditcards

--- view/frontend/web/js/view/payment/method-renderer/creditcards.js
+++ view/frontend/web/js/view/payment/method-renderer/creditcards.js
@@ -43,15 +43,18 @@ define(
         'Magento_Checkout/js/view/payment/default',
         'Magento_Checkout/js/model/payment/additional-validators',
         'TIG_Buckaroo/js/action/place-order',
+        'Magento_Checkout/js/model/quote',
         'ko',
         'Magento_Checkout/js/checkout-data',
-        'Magento_Checkout/js/action/select-payment-method'
+        'Magento_Checkout/js/action/select-payment-method',
+        'BuckarooClientSideEncryption'
     ],
     function (
         $,
         Component,
         additionalValidators,
         placeOrderAction,
+        quote,
         ko,
         checkoutData,
         selectPaymentMethodAction
@@ -61,7 +64,12 @@ define(
         return Component.extend(
             {
                 defaults: {
-                    template: 'TIG_Buckaroo/payment/tig_buckaroo_creditcards'
+                    template: 'TIG_Buckaroo/payment/tig_buckaroo_creditcards',
+                    CardNumber: null,
+                    Cvc: null,
+                    CardHolderName: null,
+                    ExpirationYear: null,
+                    ExpirationMonth: null
                 },
                 paymentFeeLabel : window.checkoutConfig.payment.buckaroo.creditcards.paymentFeeLabel,
                 currencyCode : window.checkoutConfig.quoteData.quote_currency_code,
@@ -78,12 +86,51 @@ define(
                     return this._super(options);
                 },
 
+                initObservable: function () {
+                    /** Observed fields **/
+                    this._super().observe(
+                        [
+                            'CardNumber',
+                            'Cvc',
+                            'CardHolderName',
+                            'ExpirationYear',
+                            'ExpirationMonth'
+                        ]
+                    );
+
+                    /** Check used to see if input is valid **/
+                    this.buttoncheck = ko.computed(
+                        function () {
+                            return (
+                                this.CardNumber() !== null &&
+                                this.Cvc() !== null &&
+                                this.CardHolderName() !== null &&
+                                this.ExpirationYear() !== null &&
+                                this.ExpirationMonth() !== null &&
+                                this.validate(this.country)
+                            );
+                        },
+                        this
+                    );
+
+                    return this;
+                },
+
                 selectPaymentMethod: function () {
                     window.checkoutConfig.buckarooFee.title(this.paymentFeeLabel);
 
                     selectPaymentMethodAction(this.getData());
                     checkoutData.setSelectedPaymentMethod(this.item.method);
                     return true;
+                },
+
+                /**
+                 * Run validation function
+                 */
+                validate: function (country) {
+                    var elements = $('.' + this.getCode() + ' [data-validate]:not([name*="agreement"])');
+                    this.selectPaymentMethod();
+                    return elements.valid();
                 },
 
                 payWithBaseCurrency: function () {
@@ -132,6 +179,19 @@ define(
                     if (response.RequiredAction !== undefined && response.RequiredAction.RedirectURL !== undefined) {
                         window.location.replace(response.RequiredAction.RedirectURL);
                     }
+                },
+
+                encryptCardDetails: function () {
+                    // Here is where the encryption should happen.
+                    console.log('test');
+                },
+
+                getMinExpirationYear: function () {
+                    return new Date().getFullYear();
+                },
+
+                getMaxExpirationYear: function () {
+                    return new Date().getFullYear() + 10;
                 }
             }
         );
